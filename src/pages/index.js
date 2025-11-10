738064dd75e0d23ce8bf5d148cb3079ecf637b9c
Liliya Vildanova <liliya.vild@gmail.com>
2025-11-10 12:37:57 -0500

    FIx

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ const projects = [
   {
     title: "Mobile Gaming Dashboard",
     description: "Mobile Gaming Dashboard is a responsive, genre-filtered web app built with Angular and Ionic, designed to showcase dynamic game previews with animated UI elements and custom styling.",
-    link: "https://github.com/liliyadev/portfolio",
+    link: "https://gaming.liliyadev.ca/",
   },
   {
     title: "Sky Symphony",
